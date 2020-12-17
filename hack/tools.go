871aabd1ccc94f83264a20e34467f9e997d3d782
Achim Weigel <achim.weigel@sap.com>
2020-12-17 11:23:30 +0100

    fix build

---
+++ hack/tools.go
@@ -0,0 +1,15 @@
+// +build tools
+
+// Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// This package imports things required by build scripts, to force `go mod` to see them as dependencies
+package tools
+
+import (
+	_ "github.com/onsi/ginkgo/ginkgo"
+	_ "golang.org/x/lint/golint"
+	_ "github.com/golang/mock/mockgen"
+	_ "github.com/ahmetb/gen-crd-api-reference-docs"
+)
