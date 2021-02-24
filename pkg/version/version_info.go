1bb4b919efa7a27686512281de62b6f119e7da54
Achim Weigel <achim.weigel@sap.com>
2021-02-24 08:34:29 +0100

    fix version problems 2

--- pkg/version/version_info.go
+++ pkg/version/version_info.go
@@ -3,6 +3,6 @@ package version
 var (
 	LandscaperCliVersion   = ""
 	LandscaperGitVersion   = "v0.6.1-0.20210223090757-9f4fe1b3ec92"
-	LandscaperChartVersion = "v0.7.0-dev-9f4fe1b3ec92a32380ae87b2343eb9c07527bfdd"
+	LandscaperChartVersion = "v0.7.0-dev-febf39db3ec93a69a1bbb1e100422ee2434f412f"
 	ComponentCliVersion    = ""
 )
