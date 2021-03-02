43237ca791b813212e2371681f19c3ccd4459f95
enrico-kaack-comp <enrico.kaack@sap.com>
2021-03-02 10:58:50 +0100

    Added: detailed mode

--- cmd/debug/tree/transformer.go
+++ cmd/debug/tree/transformer.go
@@ -4,67 +4,113 @@ import (
 	"fmt"
 
 	lsv1alpha1 "github.com/gardener/landscaper/apis/core/v1alpha1"
+	"sigs.k8s.io/yaml"
 )
 
-func TransformToPrintableTree(installationTrees []InstallationTree) []TreeElement {
+type TransformOptions struct {
+	DetailedMode bool
+}
+
+func (t TransformOptions) TransformToPrintableTree(installationTrees []InstallationTree) ([]TreeElement, error) {
 	var printableTrees []TreeElement
 
 	for _, installationTree := range installationTrees {
-		printableTrees = append(printableTrees, transformInstallation(installationTree))
+		transformedInstalaltion, err := t.transformInstallation(installationTree)
+		if err != nil {
+			return nil, fmt.Errorf("Error in installation %s: %w", installationTree.Installation.Name, err)
+		}
+
+		printableTrees = append(printableTrees, *transformedInstalaltion)
 	}
-	return printableTrees
+	return printableTrees, nil
 }
 
-func transformInstallation(installationTree InstallationTree) TreeElement {
+func (t TransformOptions) transformInstallation(installationTree InstallationTree) (*TreeElement, error) {
 	printableNode := TreeElement{}
 
 	printableNode.Headline = fmt.Sprintf("[%s] Installation %s\n",
 		formatStatus(string(installationTree.Installation.Status.Phase)), installationTree.Installation.Name)
 
-	if installationTree.Installation.Status.LastError != nil {
-		printableNode.Description = fmt.Sprintf("Last error: %s", installationTree.Installation.Status.LastError.Message)
+	if t.DetailedMode {
+		marshaledInstallation, err := yaml.Marshal(installationTree.Installation)
+		if err != nil {
+			return nil, fmt.Errorf("Failed marshaling instalaltion %s: %w", installationTree.Installation.Name, err)
+		}
+		printableNode.Description = string(marshaledInstallation)
+
+	} else {
+		if installationTree.Installation.Status.LastError != nil {
+			printableNode.Description = fmt.Sprintf("Last error: %s", installationTree.Installation.Status.LastError.Message)
+		}
 	}
 
 	if len(installationTree.SubInstallations) > 0 {
 		for _, inst := range installationTree.SubInstallations {
-			printableNode.Childs = append(printableNode.Childs, transformInstallation(inst))
+			transformedInstalaltion, err := t.transformInstallation(inst)
+			if err != nil {
+				return nil, fmt.Errorf("Error in installation %s: %w", installationTree.Installation.Name, err)
+			}
+			printableNode.Childs = append(printableNode.Childs, *transformedInstalaltion)
 		}
 	}
 
 	if installationTree.Execution != nil {
-		printableNode.Childs = append(printableNode.Childs, transformExecution(*installationTree.Execution))
+		transformedExecution, err := t.transformExecution(*installationTree.Execution)
+		if err != nil {
+			return nil, fmt.Errorf("Error in installation %s: %w", installationTree.Installation.Name, err)
+		}
+		printableNode.Childs = append(printableNode.Childs, *transformedExecution)
 
 	}
 
-	return printableNode
+	return &printableNode, nil
 }
 
-func transformExecution(executionTree ExecutionTree) TreeElement {
+func (t TransformOptions) transformExecution(executionTree ExecutionTree) (*TreeElement, error) {
 	printableNode := TreeElement{}
 
 	printableNode.Headline = fmt.Sprintf("[%s] Execution %s\n",
 		formatStatus(string(executionTree.Execution.Status.Phase)), executionTree.Execution.Name)
+	if t.DetailedMode {
+		marshaledExecution, err := yaml.Marshal(executionTree.Execution)
+		if err != nil {
+			return nil, fmt.Errorf("Failed marshaling execution %s: %w", executionTree.Execution.Name, err)
+		}
+		printableNode.Description = string(marshaledExecution)
+	}
 
 	if len(executionTree.DeployItems) > 0 {
 		for _, depItem := range executionTree.DeployItems {
-			printableNode.Childs = append(printableNode.Childs, transformDeployItem(depItem))
+			transformedDeployItem, err := t.transformDeployItem(depItem)
+			if err != nil {
+				return nil, fmt.Errorf("Error in Execution %s: %w", executionTree.Execution.Name, err)
+			}
+			printableNode.Childs = append(printableNode.Childs, *transformedDeployItem)
 		}
 	}
 
-	return printableNode
+	return &printableNode, nil
 }
 
-func transformDeployItem(deployItemTree DeployItemTree) TreeElement {
+func (t TransformOptions) transformDeployItem(deployItemTree DeployItemTree) (*TreeElement, error) {
 	printableNode := TreeElement{}
 
 	printableNode.Headline = fmt.Sprintf("[%s] DeployItem %s\n",
 		formatStatus(string(deployItemTree.DeployItem.Status.Phase)), deployItemTree.DeployItem.Name)
 
+	if t.DetailedMode {
+		marshaledExecution, err := yaml.Marshal(deployItemTree.DeployItem)
+		if err != nil {
+			return nil, fmt.Errorf("Failed marshaling deployitem %s: %w", deployItemTree.DeployItem.Name, err)
+		}
+		printableNode.Description = string(marshaledExecution)
+	}
+
 	if deployItemTree.DeployItem.Status.LastError != nil {
 		printableNode.Description = fmt.Sprintf("Last error: %s", deployItemTree.DeployItem.Status.LastError.Message)
 	}
 
-	return printableNode
+	return &printableNode, nil
 }
 
 func formatStatus(status string) string {
