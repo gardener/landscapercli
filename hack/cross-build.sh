16541b63e17f2d70f4f2e8e0fb5446f48e3c09e0
Achim Weigel <achim.weigel@sap.com>
2021-04-07 13:43:29 +0200

    add windows binaries

--- hack/cross-build.sh
+++ hack/cross-build.sh
@@ -17,7 +17,7 @@ fi
 
 mkdir -p dist
 
-build_matrix=("linux,amd64" "darwin,amd64")
+build_matrix=("linux,amd64" "darwin,amd64" "windows,amd64")
 
 for i in "${build_matrix[@]}"; do
   IFS=',' read os arch <<< "${i}"
