9b671392ffc0f9646959a12f7107785dae990c46
Achim Weigel <achim.weigel@sap.com>
2021-07-27 11:26:56 +0200

    adapt version info

--- pkg/version/version_info.go
+++ pkg/version/version_info.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	LandscaperCliVersion   = ""
-	LandscaperGitVersion   = "v0.7.1-0.20210324110200-6d0050cc7e17"
-	LandscaperChartVersion = "v0.8.0-dev-6d0050cc7e174fbc9827362d7947d29a6e3deef7"
+	LandscaperGitVersion   = "v0.10.1"
+	LandscaperChartVersion = "v0.10.1"
 	ComponentCliVersion    = ""
 )
