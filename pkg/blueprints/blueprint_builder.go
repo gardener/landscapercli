e929fa23fe3d0293cb70949084aee6f1f05f2bc6
Achim Weigel <achim.weigel@sap.com>
2021-08-12 08:05:12 +0200

    fix integration tests

--- pkg/blueprints/blueprint_builder.go
+++ pkg/blueprints/blueprint_builder.go
@@ -109,6 +109,7 @@ func (b *BlueprintBuilder) AddDeployExecution(deployItemName string) {
 		Name: deployItemName,
 		Type: v1alpha1.GOTemplateType,
 		File: "/" + util.ExecutionFileName(deployItemName),
+		Template: v1alpha1.NewAnyJSON([]byte{}),
 	})
 }
 
