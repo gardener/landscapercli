12297b2fbc8642269b16db2d80047b821906c1fb
Günther Wannenmacher <72857276+guewa@users.noreply.github.com>
2023-06-21 16:41:45 +0200

    fix parenthesis (run-int-tests)

--- .ci/integration_test.py
+++ .ci/integration_test.py
@@ -75,7 +75,7 @@ with tempfile.TemporaryDirectory() as tmpdir:
 
     print(f'Getting shoots/adminkubeconfig subresource for "{target_cluster}" in namespace "{namespace}"')
     # kubectl --kubeconfig=/tmp/tmpsmkavxfu/service_account_kubeconfig create --raw /apis/core.gardener.cloud/v1beta1/namespaces/garden-laas/shoots/landscapercli-pr/adminkubeconfig -f AdminKubeconfigRequest.json
-    command = ["kubectl", f"--kubeconfig={service_account_kubeconfig_path"}, "create", "--raw",
+    command = ["kubectl", f"--kubeconfig={service_account_kubeconfig_path}", "create", "--raw",
                f"/apis/core.gardener.cloud/v1beta1/namespaces/{namespace}/shoots/{target_cluster}/adminkubeconfig",
                "-f", "AdminKubeconfigRequest.json"]
 
