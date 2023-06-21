3af3fdb19b3e37cbd6918df3c18cc4463403cd58
Günther Wannenmacher <72857276+guewa@users.noreply.github.com>
2023-06-21 20:41:11 +0200

    clean debug (run-int-tests)

--- .ci/integration_test.py
+++ .ci/integration_test.py
@@ -15,7 +15,7 @@ import yaml
 from util import ctx
 
 print("Starting integration test")
-print(f"current environment: {os.environ}")
+print(f"INFO current environment: {os.environ}")
 source_path = os.environ['SOURCE_PATH']
 root_path = os.getcwd()
 target_cluster = os.environ['TARGET_CLUSTER']
@@ -43,12 +43,7 @@ else:
 print(f"'helm version' PATH={os.environ['PATH']}")
 helm_version = run([os.environ['HELM_EXECUTABLE'], "version"])
 
-kubectl_client = kubectl.KubectlClient()
-if kubectl_client.int_test_tools_dir:
-    os.environ['PATH'] = f"{kubectl_client.int_test_tools_dir}:{os.environ['PATH']}"
-print(f"'kubectl version' from python")
-kubectl_client.version()
-print(f"'kubectl version' from PATH={os.environ['PATH']}")
+print(f"INFO 'kubectl version' from PATH='{os.environ['PATH']}")
 kubectl_version = run(["kubectl", "version", "--client"])
 
 os.chdir(os.path.join(root_path, source_path, "integration-test"))
@@ -63,12 +58,11 @@ with tempfile.TemporaryDirectory() as tmpdir:
     namespace = "garden-laas"
     service_account = factory.kubernetes("laas-integration-test-service-account")
     service_account_kubeconfig_path = os.path.join(tmpdir, 'service_account_kubeconfig')
-    print(f'DEBUG garden-laas service_account_kubeconfig_path={service_account_kubeconfig_path}')
+    print(f"DEBUG garden-laas service_account_kubeconfig_path={service_account_kubeconfig_path}")
     with open(service_account_kubeconfig_path, "w") as file:
         file.write(yaml.safe_dump(service_account.kubeconfig()))
 
     debug_kubeconfig_string = yaml.safe_dump(service_account.kubeconfig())
-    print(f'DEBUG garden-laas service_account.kubeconfig()="{debug_kubeconfig_string[0:20]}..."')
     
     admin_kube_config_request = f'{{"apiVersion": "authentication.gardener.cloud/v1alpha1", "kind": "AdminKubeconfigRequest", "spec": {{"expirationSeconds": {expiration_seconds}}}}}'
 
@@ -76,36 +70,19 @@ with tempfile.TemporaryDirectory() as tmpdir:
     with open(adminKubeconfigRequest, "w") as file:
         file.write(admin_kube_config_request)
 
-    print(f'Getting shoots/adminkubeconfig subresource for "{target_cluster}" in namespace "{namespace}"')
-    # kubectl --kubeconfig=/tmp/tmpsmkavxfu/service_account_kubeconfig create --raw /apis/core.gardener.cloud/v1beta1/namespaces/garden-laas/shoots/landscapercli-pr/adminkubeconfig -f AdminKubeconfigRequest.json
-    command = ["kubectl", "create", f"--kubeconfig={service_account_kubeconfig_path}", "--raw",
-               f"/apis/core.gardener.cloud/v1beta1/namespaces/{namespace}/shoots/{target_cluster}/adminkubeconfig",
-               "-f", "AdminKubeconfigRequest.json"]
-    # CompletedProcess(args=['kubectl', '--kubeconfig=/tmp/tmpl9cwh_p8/service_account_kubeconfig', 'create', '--raw', '/apis/core.gardener.cloud/v1beta1/namespaces/garden-laas/shoots/landscapercli-pr/adminkubeconfig', '-f', 'AdminKubeconfigRequest.json'], returncode=0)
-
-    print(f'  DEBUG command="{command}"')
-    commandString = f'kubectl create --kubeconfig={service_account_kubeconfig_path} --raw /apis/core.gardener.cloud/v1beta1/namespaces/{namespace}/shoots/{target_cluster}/adminkubeconfig -f {adminKubeconfigRequest}'
-    command =  commandString.split(' ')
-    print(f'  DEBUG command="{command}"')
-
+    print(f"Getting shoots/adminkubeconfig subresource for '{target_cluster}' in namespace '{namespace}'")
+    commandString = f"kubectl create --kubeconfig={service_account_kubeconfig_path} --raw /apis/core.gardener.cloud/v1beta1/namespaces/{namespace}/shoots/{target_cluster}/adminkubeconfig -f {adminKubeconfigRequest}"
     rc = subprocess.run(commandString, shell=True, check=True, capture_output=True, encoding='utf-8')
-    print(f'DEBUG rc=\n{rc}')
+
     if rc.returncode != 0:
-        raise RuntimeError(f'Could not run command "{command}"')
+        raise RuntimeError(f"Could not run command '{commandString}'")
     
-    print(f'DEBUG rc.stdout=\n{rc.stdout}')
-
-
-    print(f'Using kubeclient.execute_command...')
-
-    kubectl_client.execute_command(f'create --raw /apis/core.gardener.cloud/v1beta1/namespaces/{namespace}/shoots/{target_cluster}/adminkubeconfig -f {adminKubeconfigRequest}', service_account_kubeconfig_path)
-
     rc_json = json.loads(rc.stdout)
     kubeconfig_bytes = base64.b64decode(rc_json["status"]["kubeconfig"])
     landscape_kubeconfig = kubeconfig_bytes.decode('utf-8')
 
 if landscape_kubeconfig == None:
-    raise RuntimeError(f'Error getting kubeconfig for "{target_cluster}" in namespace "{namespace}"')
+    raise RuntimeError(f"Error getting kubeconfig for '{target_cluster}' in namespace '{namespace}'")
                        
 with utils.TempFileAuto(prefix="landscape_kubeconfig_") as temp_file:
     temp_file.write(yaml.safe_dump(landscape_kubeconfig.kubeconfig()))
