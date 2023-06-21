d6d0aba50812544bddade2586d51b09a3be195b8
Günther Wannenmacher <72857276+guewa@users.noreply.github.com>
2023-06-21 16:37:05 +0200

    kubeconfig fix (run-int-tests)

--- .ci/integration_test.py
+++ .ci/integration_test.py
@@ -74,7 +74,8 @@ with tempfile.TemporaryDirectory() as tmpdir:
         file.write(admin_kube_config_request)
 
     print(f'Getting shoots/adminkubeconfig subresource for "{target_cluster}" in namespace "{namespace}"')
-    command = ["kubectl", "--kubeconfig" , service_account_kubeconfig_path, "create", "--raw",
+    # kubectl --kubeconfig=/tmp/tmpsmkavxfu/service_account_kubeconfig create --raw /apis/core.gardener.cloud/v1beta1/namespaces/garden-laas/shoots/landscapercli-pr/adminkubeconfig -f AdminKubeconfigRequest.json
+    command = ["kubectl", f"--kubeconfig={service_account_kubeconfig_path"}, "create", "--raw",
                f"/apis/core.gardener.cloud/v1beta1/namespaces/{namespace}/shoots/{target_cluster}/adminkubeconfig",
                "-f", "AdminKubeconfigRequest.json"]
 
