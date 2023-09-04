141cd622898c0a7087e5bdaf55387c294ad310fd
guewa <72857276+guewa@users.noreply.github.com>
2023-09-04 21:14:25 +0200

    Int-tests w/o static kubeconfigs (#185)
    * Int-tests w/o static kubeconfigs (run-int-tests)
    
    * fix adminkubeconfug name (run-int-tests)
    
    * fix run command (run-int-tests)
    
    * run command as array (run-int-tests)
    
    * fix typo (run-int-tests)
    
    * debug kubctl command (run-int-tests)
    
    * run with shell=True, check=True (run-int-tests)
    
    * kubeconfig fix (run-int-tests)
    
    * fix parenthesis (run-int-tests)
    
    * kubctl command reorder (run-int-tests)
    
    * debug on kubectl command  (run-int-tests)
    
    * more debug (run-int-tests)
    
    * more debug (run-int-tests)
    
    * more debug (run-int-tests)
    
    * more debug (run-int-tests)
    
    * update kubectl 1.17 to 1.27 (run-int-tests)
    
    * more debug (run-int-tests)
    
    * clean debug (run-int-tests)
    
    * kubeconfig is already string (run-int-tests)
    
    * DEBUG kubeconfig (run-int-tests)
    
    * debug kubeconfug
    
    * landscape_kubeconfig = rc_json
    
    * cleanup and slt in json
    
    * test  (run-int-tests)
    
    * update integration test image (run-int-tests)
    
    * New pr cluster and debug
    
    * cluster names updated

--- .ci/integration_test.py
+++ .ci/integration_test.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+import base64
+import json
+import subprocess
+import tempfile
 import helm
 import kubectl
 import os
@@ -11,7 +15,7 @@ import yaml
 from util import ctx
 
 print("Starting integration test")
-print(f"current environment: {os.environ}")
+print(f"INFO current environment: {os.environ}")
 source_path = os.environ['SOURCE_PATH']
 root_path = os.getcwd()
 target_cluster = os.environ['TARGET_CLUSTER']
@@ -39,24 +43,51 @@ else:
 print(f"'helm version' PATH={os.environ['PATH']}")
 helm_version = run([os.environ['HELM_EXECUTABLE'], "version"])
 
-kubectl_client = kubectl.KubectlClient()
-if kubectl_client.int_test_tools_dir:
-    os.environ['PATH'] = f"{kubectl_client.int_test_tools_dir}:{os.environ['PATH']}"
-print(f"'kubectl version' from python")
-kubectl_client.version()
-print(f"'kubectl version' from PATH={os.environ['PATH']}")
+print(f"INFO 'kubectl version' from PATH='{os.environ['PATH']}")
 kubectl_version = run(["kubectl", "version", "--client"])
 
 os.chdir(os.path.join(root_path, source_path, "integration-test"))
 
 factory = ctx().cfg_factory()
 print(f"Getting kubeconfig for {target_cluster}")
-landscape_kubeconfig = factory.kubernetes(target_cluster)
+# landscape_kubeconfig = factory.kubernetes(target_cluster)
+# see https://github.com/gardener/gardener/blob/master/docs/usage/shoot_access.md#shootsadminkubeconfig-subresource
+expiration_seconds = 86400 # is 1 day
+landscape_kubeconfig_json = None
+with tempfile.TemporaryDirectory() as tmpdir:
+    namespace = "garden-laas"
+    service_account = factory.kubernetes("laas-integration-test-service-account")
+    service_account_kubeconfig_path = os.path.join(tmpdir, 'service_account_kubeconfig')
+    print(f"DEBUG garden-laas service_account_kubeconfig_path={service_account_kubeconfig_path}")
+    with open(service_account_kubeconfig_path, "w") as file:
+        file.write(yaml.safe_dump(service_account.kubeconfig()))
 
+    debug_kubeconfig_string = yaml.safe_dump(service_account.kubeconfig())
+    
+    admin_kube_config_request = f'{{"apiVersion": "authentication.gardener.cloud/v1alpha1", "kind": "AdminKubeconfigRequest", "spec": {{"expirationSeconds": {expiration_seconds}}}}}'
+
+    adminKubeconfigRequest = 'AdminKubeconfigRequest.json'
+    with open(adminKubeconfigRequest, "w") as file:
+        file.write(admin_kube_config_request)
+
+    print(f"Getting shoots/adminkubeconfig subresource for '{target_cluster}' in namespace '{namespace}'")
+    commandString = f"kubectl create --kubeconfig={service_account_kubeconfig_path} --raw /apis/core.gardener.cloud/v1beta1/namespaces/{namespace}/shoots/{target_cluster}/adminkubeconfig -f {adminKubeconfigRequest}"
+    rc = subprocess.run(commandString, shell=True, check=True, capture_output=True, encoding='utf-8')
+
+    if rc.returncode != 0:
+        raise RuntimeError(f"Could not run command '{commandString}'")
+    
+    rc_json = json.loads(rc.stdout)
+    kubeconfig_bytes = base64.b64decode(rc_json["status"]["kubeconfig"])
+    landscape_kubeconfig_json = yaml.safe_load(kubeconfig_bytes.decode('utf-8'))
+
+if landscape_kubeconfig_json == None:
+    raise RuntimeError(f"Error getting kubeconfig for '{target_cluster}' in namespace '{namespace}'")
+                       
 with utils.TempFileAuto(prefix="landscape_kubeconfig_") as temp_file:
-    temp_file.write(yaml.safe_dump(landscape_kubeconfig.kubeconfig()))
+    json.dump(landscape_kubeconfig_json, temp_file)
     landscape_kubeconfig_path = temp_file.switch()
-
+    
     command = ["go", "run", "main.go",
             "--kubeconfig", landscape_kubeconfig_path,
             "--landscaper-namespace", "lndscpr-int-test",
@@ -80,3 +111,4 @@ with utils.TempFileAuto(prefix="landscape_kubeconfig_") as temp_file:
 
     if run.returncode != 0:
         raise EnvironmentError("Integration test exited with errors")
+    
