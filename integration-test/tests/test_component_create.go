18f4f4ffd0a63fbeae578f02e457545d4b220ee2
Achim Weigel <achim.weigel@sap.com>
2024-03-01 09:09:31 +0100

    fetch latest release

--- integration-test/tests/test_component_create.go
+++ integration-test/tests/test_component_create.go
@@ -170,7 +170,7 @@ func (t *componentCreateTest) addHelmDeployItemWithExternalChart(ctx context.Con
 		"--component-directory",
 		t.componentDir,
 		"--oci-reference",
-		"eu.gcr.io/gardener-project/landscaper/tutorials/charts/ingress-nginx:4.0.17",
+		"eu.gcr.io/gardener-project/landscaper/integration-tests/charts/hello-world:1.0.0",
 		"--resource-version",
 		"v0.2.0",
 		"--cluster-param",
