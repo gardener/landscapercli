7e856950f5659aabcedfe649675a52bee3e9f5ad
Robert Graeff <robert.graeff@sap.com>
2025-09-09 08:06:34 +0200

    fix: fix collector unit test

--- cmd/installations/inspect/collector_test.go
+++ cmd/installations/inspect/collector_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gardener/landscaper/test/utils/envtest"
 	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestCollector(t *testing.T) {
@@ -15,14 +16,19 @@ func TestCollector(t *testing.T) {
 		K8sClient: fakeClient,
 	}
 	expectedInstallationAgg := state.Installations["inttest/my-aggregation"]
+	expectedInstallationAgg.TypeMeta = metav1.TypeMeta{}
 	expectedInstallationServer := state.Installations["inttest/server-gw64l"]
+	expectedInstallationServer.TypeMeta = metav1.TypeMeta{}
 	expectedInstallationIngress := state.Installations["inttest/ingress-hhsjf"]
-
+	expectedInstallationIngress.TypeMeta = metav1.TypeMeta{}
 	expectedExecutionServer := state.Executions["inttest/server-gw64l"]
+	expectedExecutionServer.TypeMeta = metav1.TypeMeta{}
 	expectedExecutionIngress := state.Executions["inttest/ingress-hhsjf"]
-
+	expectedExecutionIngress.TypeMeta = metav1.TypeMeta{}
 	expectedDeployitemServer := state.DeployItems["inttest/server-gw64l-deploy-7mhc2"]
+	expectedDeployitemServer.TypeMeta = metav1.TypeMeta{}
 	expectedDeployitemIngress := state.DeployItems["inttest/ingress-hhsjf-deploy-kptjl"]
+	expectedDeployitemIngress.TypeMeta = metav1.TypeMeta{}
 
 	actualStructure, err := collector.CollectInstallationsInCluster("", "inttest")
 	assert.NoError(t, err)
@@ -64,6 +70,7 @@ func TestCollector(t *testing.T) {
 	actualStructure, err = collector.CollectInstallationsInCluster("", "*")
 	assert.NoError(t, err)
 	expectedFakeInstallation := state.Installations["default/fakeinst"]
+	expectedFakeInstallation.TypeMeta = metav1.TypeMeta{}
 	// verify that all of the previous installations as well as the fakeinst from the default namespace are contained in the actual structure
 	t.Run("Collection of landscaper CR (installations, executions, deployitems) across all namespaces", func(t *testing.T) {
 		assert.Contains(t, actualStructure, &InstallationTree{Installation: expectedFakeInstallation})
@@ -71,5 +78,4 @@ func TestCollector(t *testing.T) {
 			assert.Contains(t, actualStructure, it)
 		}
 	})
-
 }
